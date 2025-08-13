73288e5bdf02062120ff46280d2de8d9c5423665
karatalov <nursultankaratalov2@gmail.com>
2025-08-14 03:12:22 +0600

    sale section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Actions from '@/Components/Actions'
 import Slider from '@/Components/Slider/Slider'
 
 export default function Home() {
 	return (
 		<main className='w-full mx-auto mb-20'>
 			<Slider />
+			<div className='px-[max(12px,calc((100%-1208px)/2))] flex flex-col gap-y-20 md:mb-25 xl:mb-30'>
+				<Actions />
+			</div>
 		</main>
 	)
 }
